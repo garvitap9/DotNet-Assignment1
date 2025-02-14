e87374ac5007a22bab20f6aba7cdd7917a1e6eb1
garvita <garvita.pun9@gmail.com>
2025-02-14 12:01:47 +0530

    updated

---
+++ FibonacciSeriesUpto8Elements/FibonacciSeriesGeneration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSeriesUpto8Elements
+{
+    public class FibonacciSeriesGeneration
+    {
+        public int firstNumber = 0;
+        public int secondNumber = 1;
+        public int nextNumber;
+        public int totalElements = 8;
+
+        public void FibonacciSeriesLogic()
+        {
+            Console.WriteLine("Printing Fibonacci Series upto 8 elements like 1 1 2 3 5 8 13 21 :");
+            Console.WriteLine();
+            Console.Write($"{secondNumber} ");
+            for(int i =1; i<totalElements; ++i)
+            {
+                nextNumber = firstNumber + secondNumber;
+                Console.Write(nextNumber + " ");
+                firstNumber = secondNumber;
+                secondNumber = nextNumber;
+            }
+        }
+
+    }
+}
---
+++ FibonacciSeriesUpto8Elements/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSeriesUpto8Elements
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FibonacciSeriesGeneration fibonacciSeriesGeneration = new FibonacciSeriesGeneration();
+            fibonacciSeriesGeneration.FibonacciSeriesLogic();
+        }
+    }
+}
